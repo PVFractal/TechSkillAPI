23ae47431b21772624f8c03fa8e82c28b81e3ac5
Peter Vahlberg <peter.vahlberg@jpgreze.com>
2025-05-20 17:20:42 -0700

    Added another skill

--- obj/Release/net9.0/TechSkillAPI.AssemblyInfo.cs
+++ obj/Release/net9.0/TechSkillAPI.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechSkillAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e73b1f38130623cb9e062cc65cff19b0bdcacc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechSkillAPI")]
